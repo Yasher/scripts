aa16b7793dfb912eb66911096cb2c8a33a3985dd
Yasher <x_mybox@mail.ru>
2026-03-27 22:06:08 +0800

    Update bad_bot.sh

--- bad_bot.sh
+++ bad_bot.sh
@@ -13,7 +13,7 @@ MAP_CONTENT='map $http_user_agent $bad_bot {
 }'
 
 IF_BLOCK='
-if ($http_user_agent ~* (PerplexityBot|FriendlyCrawler|GPTBot|AhrefsBot|Amazonbot|PetalBot|SemrushBot|MJ12bot|Riddler|aiHitBot|trovitBot|Detectify|BLEXBot|dotbot|FlipboardProxy|rogerBot|LinkpadBot|Bytespider|Serpstatbot|ClaudeBot|Applebot)) {
+if ($http_user_agent ~* (meta-externalagent|PerplexityBot|FriendlyCrawler|GPTBot|AhrefsBot|Amazonbot|PetalBot|SemrushBot|MJ12bot|Riddler|aiHitBot|trovitBot|Detectify|BLEXBot|dotbot|FlipboardProxy|rogerBot|LinkpadBot|Bytespider|Serpstatbot|ClaudeBot|Applebot)) {
     return 410;
 }
 '
