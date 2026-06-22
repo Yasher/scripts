dbb65e37ffa01a3b62b5259b97c0791de09c4e27
Yasher <x_mybox@mail.ru>
2026-06-22 23:02:50 +0800

    Update go_isp.sh

--- go_isp.sh
+++ go_isp.sh
@@ -2,13 +2,13 @@
 #!/bin/bash
 
 
-rawtarget=$(xclip -o)
+ip=$(ss -pant| grep ihttp| awk '{printf $4}'| awk -F : '{printf $1}')
 
-ip=$rawtarget
+port=$(ss -pant| grep ihttp| awk '{printf $4}'| awk -F : '{printf $2}')
 
 key=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32)
 
 /usr/local/mgr5/sbin/mgrctl -m ispmgr session.newkey username=root key=$key
 
-url="https://$ip:1500/ispmgr?func=auth&username=root&key=$key&checkcookie=noht"
+url="https://$ip:$port/ispmgr?func=auth&username=root&key=$key&checkcookie=noht"
 echo $url
