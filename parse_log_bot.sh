25e607dd43a214b3b77a4e1e1124eb09e3eec798
Yasher <x_mybox@mail.ru>
2026-06-23 03:05:19 +0800

    Update parse_log_bot.sh

--- parse_log_bot.sh
+++ parse_log_bot.sh
@@ -1,90 +1,4 @@
 #!/bin/bash
-while true; do
-
-httpd_logs_dir="/var/www/httpd-logs"
-nginx_logs_dir="/var/log/nginx"
-apache2_logs_dir="/var/log/apache2"
-httpd_logs2_dir="/var/log/httpd"
-www_root_logs_dirs=(/var/www/*/data/logs)
-
-# Заполняем список файлов кроме /var/www/*/data/logs
-main_files=()
-while IFS= read -r -d '' file; do
-    main_files+=("$file")
-done < <(find "$httpd_logs_dir" -type f -name "*access.log" -print0 2>/dev/null)
-
-while IFS= read -r -d '' file; do
-    main_files+=("$file")
-done < <(find "$nginx_logs_dir" -type f -name "*access.log" -print0 2>/dev/null)
-
-while IFS= read -r -d '' file; do
-    main_files+=("$file")
-done < <(find "$apache2_logs_dir" -type f -name "*access.log" -print0 2>/dev/null)
-
-while IFS= read -r -d '' file; do
-    main_files+=("$file")
-done < <(find "$httpd_logs2_dir" -type f -name "*access.log" -print0 2>/dev/null)
-
-# Добавляем пункт для директорий и ручного ввода
-main_files+=("/var/www/*/data/logs")
-#main_files+=("n) Ввести путь вручную")
-
-echo "Найдены следующие лог-файлы:"
-for i in "${!main_files[@]}"; do
-    printf "%3d) %s\n" "$i" "${main_files[$i]}"
-done
-
-echo "n) Ввести путь вручную"
-echo "q) Выход"
-echo -n "Введите номер для анализа: "
-read main_choice
-
-
-if [[ "$main_choice" == "q" ]]; then
-    break
-fi
-
-
-
-# --- Новый пункт: ручной ввод пути ---
-#if [ "$selected_main" = "Ввести путь вручную" ]; then
-if [[ "$main_choice" == "n" ]]; then
-    echo -n "Введите полный путь к лог-файлу: "
-    read manual_path
-    if [ ! -f "$manual_path" ]; then
-        echo "Файл не найден: $manual_path"
-        continue
-    fi
-    selected_file="$manual_path"
-
-
-if [[ "$main_choice" != "n" ]] && \
-(! [[ "$main_choice" =~ ^[0-9]+$ ]] || [ "$main_choice" -lt 0 ] || [ "$main_choice" -ge "${#main_files[@]}" ] ); then
-    echo "Неверный номер."
-    continue
-fi
-
-
-selected_main="${main_files[$main_choice]}"
-
-
-
-# --- Выбор из /var/www/*/data/logs ---
-elif [ "$selected_main" = "/var/www/*/data/logs" ]; then
-    sub_files_unsorted=()
-    for dir in "${www_root_logs_dirs[@]}"; do
-        [ -d "$dir" ] || continue
-        while IFS= read -r -d '' file; do
-            sub_files_unsorted+=("$file")
-        done < <(find "$dir" -type f \( -name "*access.log" -o -name "*.access.log*.gz" \) -print0 2>/dev/null)
-    done
-
-    # Сортировка natural version
-    IFS=$'\n' sorted_files=($(printf "%s\n" "${sub_files_unsorted[@]}" | sort -V))
-    unset IFS
-
-    if [ ${#sorted_files[@]} -eq 0 ]; then
-        echo "В директориях нет подходящих файлов."
         continue
     fi
 
@@ -118,17 +32,47 @@ else
 fi
 
 # Команда анализа
-
 if [[ "$selected_file" == *.gz ]]; then
-    cmd="zcat \"$selected_file\" \
-    | awk '\$9 !~ /^(403|444|410|429)$/' \
-    | grep -oiE '\\b[a-zA-Z0-9./;+_-]*(bot|meta|facebook)[a-zA-Z0-9./;+_-]*\\b' \
-    | sort | uniq -c | sort -nr | head -n20"
+    cmd="zcat \"$selected_file\" | awk '
+    !/\" (403|444|410|429) / {
+        line=tolower(\$0)
+
+        while (match(line, /[a-z0-9.\/;+_-]*(bot|meta|facebook)[a-z0-9.\/;+_-]*/)) {
+            token=substr(line, RSTART, RLENGTH)
+            cnt[token]++
+            line=substr(line, RSTART+RLENGTH)
+        }
+    }
+    END {
+        for (i in cnt)
+            print cnt[i], i
+    }' | sort -nr | head -20"
 else
-    cmd="awk '\$9 !~ /^(403|444|410|429)$/' \"$selected_file\" \
-    | grep -oiE '\\b[a-zA-Z0-9./;+_-]*(bot|meta|facebook)[a-zA-Z0-9./;+_-]*\\b' \
-    | sort | uniq -c | sort -nr | head -n20"
+    cmd="awk '
+    !/\" (403|444|410|429) / {
+        line=tolower(\$0)
+
+        while (match(line, /[a-z0-9.\/;+_-]*(bot|meta|facebook)[a-z0-9.\/;+_-]*/)) {
+            token=substr(line, RSTART, RLENGTH)
+            cnt[token]++
+            line=substr(line, RSTART+RLENGTH)
+        }
+    }
+    END {
+        for (i in cnt)
+            print cnt[i], i
+    }' \"$selected_file\" | sort -nr | head -20"
 fi
+#if [[ "$selected_file" == *.gz ]]; then
+#    cmd="zcat \"$selected_file\" \
+#    | awk '\$9 !~ /^(403|444|410|429)$/' \
+#    | grep -oiE '\\b[a-zA-Z0-9./;+_-]*(bot|meta|facebook)[a-zA-Z0-9./;+_-]*\\b' \
+#    | sort | uniq -c | sort -nr | head -n20"
+#else
+#    cmd="awk '\$9 !~ /^(403|444|410|429)$/' \"$selected_file\" \
+#    | grep -oiE '\\b[a-zA-Z0-9./;+_-]*(bot|meta|facebook)[a-zA-Z0-9./;+_-]*\\b' \
+#    | sort | uniq -c | sort -nr | head -n20"
+#fi
 
 #cmd="$grep_cmd -oiE '\"[^\"]+\"' \"$selected_file\" | $grep_cmd -oiE '\\b[a-zA-Z0-9./;+_-]*(bot|meta|facebook)[a-zA-Z0-9./;+_-]*\\b' | sort | uniq -c | sort -nr | head -n20"
 
